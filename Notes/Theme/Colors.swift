98f06b225e04655533f1b0e1b44d42c8a21742c7
Aleksey Efimov <efimov.may@yandex.ru>
2024-01-29 16:49:11 +0300

    Заполнены Colors, Sizes

--- Notes/Theme/Colors.swift
+++ Notes/Theme/Colors.swift
@@ -5,4 +5,19 @@
 //  Created by Aleksey Efimov on 29.01.2024.
 //
 
-import Foundation
+import UIKit
+
+enum Colors {
+	static let white = UIColor.color(
+		light: UIColor(hex: 0xFFFFFF),
+		dark: UIColor(hex: 0x000000)
+	)
+	static let black = UIColor.color(
+		light: UIColor(hex: 0x000000),
+		dark: UIColor(hex: 0xFFFFFF)
+	)
+}
+
+enum Theme {
+	static let mainColor = Colors.white
+}
