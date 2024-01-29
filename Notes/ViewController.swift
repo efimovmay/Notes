455159e03d4b1ee86d9132062bb4296a0d0df1c2
Aleksey Efimov <efimov.may@yandex.ru>
2024-01-29 15:32:39 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Aleksey Efimov on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
